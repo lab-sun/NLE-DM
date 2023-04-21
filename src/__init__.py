29b7d43a5825450fccfa338bb3bf40352cd406fe
Yuchao Feng <99387180+fyctime052@users.noreply.github.com>
2023-04-21 20:57:17 +0800

    Delete src directory

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pretrain_model import deeplabv3_resnet50, deeplabv3_resnet101, deeplabv3_mobilenetv3_small, deeplabv3_mobilenetv3_large
-from .act_rea_model import act_exp
-from .act_des_model import act_des_resnet50, act_des_resnet101, act_des_mobile_large, act_des_mobile_small
-
