fc5e60b68df9f439df67961189e711c94a407eda
Yuchao Feng <99387180+fyctime052@users.noreply.github.com>
2023-06-06 19:14:08 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,5 @@
 from .pretrain_model import deeplabv3_resnet50, deeplabv3_resnet101, \
     deeplabv3_mobilenetv3_small, deeplabv3_mobilenetv3_large
-
-
 from .act_rea_model import act_exp
-from .act_rea_ablation_model import act_exp_ablation
-from .act_rea_ablation_model_mobile import act_exp_ablation_mobile
-
-
 from .act_des_model import act_des_resnet50, act_des_resnet101, act_des_resnet50_small
 from .act_des_model_mobile import act_des_mobile_large, act_des_mobile_small
-from .act_des_ablation_model import act_des_ablation
